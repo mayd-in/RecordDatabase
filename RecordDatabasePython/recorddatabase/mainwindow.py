7efc6cb59cca3ebaab735a60810c73d371742079
maydin <mma-95@hotmail.com>
2021-03-18 02:47:16 +0300

    Added textEdit and help menu

--- RecordDatabasePython/recorddatabase/mainwindow.py
+++ RecordDatabasePython/recorddatabase/mainwindow.py
@@ -2,4 +2,24 @@ from PyQt5.QtWidgets import *
 
 
 class MainWindow(QMainWindow):
-    pass
+    def __init__(self):
+        super().__init__()
+
+        textEditor = QTextEdit()
+        self.setCentralWidget(textEditor)
+
+        self.setupMenus()
+
+        # PROPERTIES
+        self.textEditor = textEditor
+
+    def setupMenus(self):
+        # HELP MENU
+        helpMenu = QMenu(self.tr("&Help"), self)
+        self.menuBar().addMenu(helpMenu)
+
+        # CONNECTIONS
+        helpMenu.addAction(self.tr("&Help"), lambda: QMessageBox.about(self, QApplication.applicationDisplayName(),
+                    self.tr("<p><b>Record Database Editor</b> allows storing "
+                       "per contact information in files using a database.</p>")))
+        helpMenu.addAction(self.tr("About Qt"), QApplication.aboutQt)
